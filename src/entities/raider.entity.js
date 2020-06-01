e03e7bd0b3c75145f79533dc3a4fc154a9789bd5
two-as-one <furka.thorne@gmail.com>
2020-06-01 17:58:33 +0100

    added stance changes to raider AI

--- src/entities/raider.entity.js
+++ src/entities/raider.entity.js
@@ -1,6 +1,8 @@
 import { EntityAI } from "./_entity.ai"
 import { PainEffect } from "../combat-effects/pain.effect"
 import { LoveEffect } from "../combat-effects/love.effect"
+import { FightEffect } from "../combat-effects/fight.effect"
+import { ForeplayEffect } from "../combat-effects/foreplay.effect"
 
 export class Raider extends EntityAI {
   get name() {
@@ -19,6 +21,18 @@ export class Raider extends EntityAI {
         effect: new LoveEffect(this.game, this, { value: 1 }),
         applies: () => {
           return this.love > this.pain
+        },
+      },
+      {
+        effect: new FightEffect(this.game, this, {}),
+        applies: () => {
+          return this.pain > this.love && this.game.scene.stance === "foreplay"
+        },
+      },
+      {
+        effect: new ForeplayEffect(this.game, this, {}),
+        applies: () => {
+          return this.love > this.pain && this.game.scene.stance === "fight"
         },
       },
     ]
